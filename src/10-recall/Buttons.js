33fd5f36185dd42b7d3727373ac1e918b9bbb3b7
Igor Lukjanov <lukjanov.igr@gmail.com>
2022-09-15 09:42:15 +0300

    Show clicked button task

--- src/10-recall/Buttons.js
+++ src/10-recall/Buttons.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Buttons({ number }) {
-  return <button>{number}</button>;
+function Buttons() {
+  const [showButton, setShowButton] = useState('');
+
+  const printNumber = (index) => {
+    setShowButton(index);
+  };
+
+  return (
+    <>
+      <h2>{showButton}</h2>
+      <div className="buttons-container">
+        {[...Array(50)].map((_, i) => (
+          <button key={i} onClick={() => printNumber(i + 1)}>
+            {i + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Buttons;
--- src/10-recall/Main.js
+++ src/10-recall/Main.js
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Main.scss';
 import Square from './Square';
 import Buttons from './Buttons';
 
 function Main() {
-  const [buttonsLength, setButtonsLength] = useState(50);
-  // const sayBu = () => {
-  //   console.log('%c Buuuu ', 'color: red');
-  // };
-
   return (
     <div>
       <Square color="red" />
-      <div className="buttons-container">
-        {[...Array(buttonsLength)].map((_, i) => (
-          <Buttons number={i + 1} />
-        ))}
-      </div>
+      <Buttons />
     </div>
   );
 }
